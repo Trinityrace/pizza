d5984fd07bf7e4bbf68e30a48d2cc3d38d2a2ff3
Trinity John <trinityrtime@gmail.com>
2019-11-11 04:36:25 +0300

    htnl complete input with buttons and images and rows with columns

---
+++ js/js.js
@@ -0,0 +1,15 @@
+document.getElementById("myBtn"
+).onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("myDropdown"
+  ).classList.toggle("show");
+}
+
+document.getElementById("myBTn")
+.onclick=function(){myFunction()};
+
+function  myFunction() {
+  document.getElementById("myDropddd"
+  ).classList.toggle("show");
+}
